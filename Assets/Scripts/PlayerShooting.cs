eeb12e85b30dcffc699255bec484c910d13f832c
ThunderNinja25 <161791737+ThunderNinja25@users.noreply.github.com>
2024-07-16 20:57:56 -0700

    Lesson1

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] private float shootingStrength;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform shootingPoint;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            ShootBullet();
+        }
+    }
+
+    private void ShootBullet()
+    {
+        GameObject bulletClone = Instantiate(bullet, shootingPoint.position, shootingPoint.rotation);
+        bulletClone.GetComponent<Rigidbody>().AddForce(bulletClone.transform.forward * shootingStrength, ForceMode.VelocityChange);
+        Destroy(bulletClone, 3f);
+    }
+}
